f1be2c8d6dec0bfb6c0dfbec887c01e40b329175
Mohamed Aymen <aymen.kernel@gmail.com>
2024-09-28 16:53:53 +0100

    add / to show api version

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-ini_set('display_errors', 'on');
-error_reporting(E_ALL);
+header('Access-Control-Allow-Origin: *');
 
 require_once __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/config.php';
@@ -10,6 +9,10 @@ use  \Symfony\Component\HttpFoundation\JsonResponse;
 $app = new App\APIChoufli7allApp();
 $container = $app->getContainer();
 
+$app->get('/', function () {
+    new JsonResponse(['version' => '1.0']);
+});
+
 $app->get('random', function () use ($container) {
     $quote = [];
     $stmt = $container['pdo']->query('SELECT COUNT(*) AS count FROM quotes');
@@ -18,7 +21,7 @@ $app->get('random', function () use ($container) {
     if ($count > 0) {
         // Get a random quote
         $randomIndex = rand(0, $count - 1);
-        $stmt = $container['pdo']->prepare('SELECT quotes.quotes_text, actors.actors_name FROM quotes JOIN actors ON quotes.id_author = actors.actors_id LIMIT 1 OFFSET :offset');
+        $stmt = $container['pdo']->prepare('SELECT quotes.quotes_text, actors.actors_slug, actors.actors_name FROM quotes JOIN actors ON quotes.id_author = actors.actors_id LIMIT 1 OFFSET :offset');
         $stmt->bindValue(':offset', $randomIndex, PDO::PARAM_INT);
         $stmt->execute();
 
@@ -27,6 +30,7 @@ $app->get('random', function () use ($container) {
         $quote = [
             'quote' => $row['quotes_text'],
             'actor' => $row['actors_name'],
+            'actor_slug' => $row['actors_slug'],
         ];
     }
 
